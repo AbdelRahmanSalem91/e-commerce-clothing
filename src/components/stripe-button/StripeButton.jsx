0115405eb7552a6e94a68a540820fa86a131d1fd
Abd El Rahman Ahmed <abdoahmed424@gmail.com>
2022-01-19 18:49:49 +0200

    adding stripe and updating title and icon

---
+++ src/components/stripe-button/StripeButton.jsx
@@ -0,0 +1,29 @@
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51KJgHbEbjF631EDBfqwC2SUijoyJKeKGgQlJmJVoU1oKtlKE1LXlABWIBfg5tOIjrNJR83dEKQ5Ayevn83D7ch5s00RHMnPA1o";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Total: $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeButton;
